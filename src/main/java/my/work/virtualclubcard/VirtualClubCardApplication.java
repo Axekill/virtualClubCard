966082b46d1c4ce6cdda96e87e5bb7ce31397521
ГусевАЮ <andrei3.ru@ya.ru>
2024-10-25 15:31:45 +0300

    first commit

---
+++ src/main/java/my/work/virtualclubcard/VirtualClubCardApplication.java
@@ -0,0 +1,13 @@
+package my.work.virtualclubcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClubCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualClubCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/work/virtualclubcard/VirtualClubCardApplicationTests.java
@@ -0,0 +1,13 @@
+package my.work.virtualclubcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClubCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
